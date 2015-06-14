696c2d961ed882c5938bee8a6d496f5e2e26473c
HadorX <rreszka.pawel@gmail.com>
2015-06-14 15:18:00 +0200

    Part of modifications for energy function

---
+++ SeamCarving/SeamCarvingCore/EnergyFunctionBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeamCarvingCore
+{
+    abstract class EnergyFunctionBase
+    {
+        public int[,] Energy;
+
+        public int[,,] Pixels;
+
+        public int Width;
+
+        public int Height;
+
+        protected abstract int GetPixelEnergy(int x, int y);
+        public void ComputeEnergy(int width, int height, int [,,] pixels)
+        {
+            Width = width;
+            Height = height;
+            Pixels = pixels;
+
+            Energy = new int[width, height];
+
+            Parallel.For(0, width, x =>
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    Energy[x, y] = GetPixelEnergy(x, y);
+                }
+            });
+        }
+
+        protected int GetPixelData(int x, int y, int color)
+        {
+            if (x < 0 || x >= Width || y < 0 || y >= Height)
+                return 0;
+            return Pixels[x, y, color];
+        }
+    }
+}
---
+++ SeamCarving/SeamCarvingCore/Prewitt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeamCarvingCore
+{
+    class Prewitt : EnergyFunctionBase
+    {
+        protected override int GetPixelEnergy(int x, int y)
+        {
+            int[] pixels = new int[8];
+
+            pixels[0] = (byte)GetPixelValue(bitmapData, x - 1, y - 1, size);
+            pixels[1] = (byte)GetPixelValue(bitmapData, x, y - 1, size);
+            pixels[2] = (byte)GetPixelValue(bitmapData, x + 1, y - 1, size);
+            pixels[3] = (byte)GetPixelValue(bitmapData, x - 1, y, size);
+            pixels[4] = (byte)GetPixelValue(bitmapData, x + 1, y, size);
+            pixels[5] = (byte)GetPixelValue(bitmapData, x - 1, y + 1, size);
+            pixels[6] = (byte)GetPixelValue(bitmapData, x, y + 1, size);
+            pixels[7] = (byte)GetPixelValue(bitmapData, x + 1, y + 1, size);
+
+            int xPrewitt = pixels[2] + pixels[4] + pixels[7] - pixels[0] - pixels[3] - pixels[5];
+            int yPrewitt = pixels[5] + pixels[6] + pixels[7] - pixels[0] - pixels[1] - pixels[2];
+
+            int priwitt = Math.Abs(xPrewitt) + Math.Abs(yPrewitt);
+
+
+            if (priwitt > 255)
+            {
+                priwitt = 255;
+            }
+
+            return priwitt;
+        }
+
+}
+}
